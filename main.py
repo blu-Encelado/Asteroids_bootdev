d06cf4afe77b2c9087d43adb685c90eb019efcbd
blu-Encelado <bluencelado@gmail.com>
2025-09-20 17:19:01 +0200

    Asteroid 20/09 on game loop, adding circleshape

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
-pygame.init()
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+#pygame.init()
+#screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+dt = 0
 
-def starting_game():
+def game_loop():
     number = 1
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while number != 0:
+    clock = pygame.time.Clock()
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen,(0,0,0))
+        dt = clock.tick(60) / 1000
 
 def main():
     pygame.init()
-    starting_game()
+    clock = pygame.time.Clock
+    
+    game_loop()
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
